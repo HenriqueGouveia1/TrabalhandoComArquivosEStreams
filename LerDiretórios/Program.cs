9a03f79002b42bf45e74e7f2732d5f7b96f261a1
henriquegouveia1 <henriquegs002@gmail.com>
2022-05-27 20:54:21 -0300

    método que tras informações do diretorio criado

--- LerDiretórios/Program.cs
+++ LerDiretórios/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var path = @"C:\github projects\Curso Trabalhando com Arquivos e Streams em C#\Directory_and_DirectoryInfo\globo";
+
+LerDiretorios(path);
+
+Console.WriteLine("Digite [enter] para finalizar");
+Console.ReadLine();
+
+static void LerDiretorios(string path)
+{
+    if (Directory.Exists(path))
+    {
+        var diretorios = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+
+        foreach (var dir in diretorios)
+        {
+            var dirInfo = new DirectoryInfo(dir);
+            Console.WriteLine($"[Nome]: {dirInfo.Name}");
+        }
+    }
+
+
+}
