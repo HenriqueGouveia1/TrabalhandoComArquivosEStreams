d74656a0bf73a3ed208753977ccbffbffafb80c2
henriquegouveia1 <henriquegs002@gmail.com>
2022-05-27 20:21:11 -0300

    Criando os diretórios representando as américas

--- Directory_and_DirectoryInfo/Program.cs
+++ Directory_and_DirectoryInfo/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var path = Path.Combine(Environment.CurrentDirectory, "globo");
+var dirGLobo = Directory.CreateDirectory(path);
+
+var dirAmericaNorte = dirGLobo.CreateSubdirectory("América do Norte");
+var dirAmericaSul = dirGLobo.CreateSubdirectory("América do Sul");
+var dirAmericaCentro = dirGLobo.CreateSubdirectory("América Central");
+
+dirAmericaCentro.CreateSubdirectory("Costa Rica");
+
+dirAmericaNorte.CreateSubdirectory("USA");
+
+dirAmericaSul.CreateSubdirectory("Brasil");
