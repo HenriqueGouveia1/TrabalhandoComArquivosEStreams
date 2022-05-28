a8735a45398c130c4e58b197b5cdd5b54bfbcea1
henriquegouveia1 <henriquegs002@gmail.com>
2022-05-28 13:02:21 -0300

    Agora utilizando outro delimitador

--- csvHelper/Program.cs
+++ csvHelper/Program.cs
@@ -2,23 +2,90 @@
 using CsvHelper.Configuration;
 using CsvHelper;
 
-var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "produtos.csv");
+//LerCSVComDynamic();
+//LerCsvComClasse();
+LerCsvComOutroDelimiter();
 
-var fi = new FileInfo(path);
+Console.WriteLine("[Enter] para finalizar");
+Console.ReadLine();
 
-if (!fi.Exists)
-    throw new FileNotFoundException($"O arquivo {path} não existe");
-
-using var sr = new StreamReader(fi.FullName);
-var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
-using var csvReader = new CsvReader(sr, csvConfig);
-
-var registros = csvReader.GetRecords<dynamic>(); //não tem intelisense, precisa ser o nome exato
-
-foreach (var registro in registros)
+static void LerCsvComOutroDelimiter()
 {
-    Console.WriteLine($"nome:{registro.Nome}");
-    Console.WriteLine($"email:{registro.Email}");
-    Console.WriteLine($"telefone:{registro.Telefone}");
-    Console.WriteLine("--------");
+    var path = Path.Combine(
+        Environment.CurrentDirectory,
+        "Entrada",
+        "Livros.csv");
+    var fi = new FileInfo(path);
+
+    if (!fi.Exists)
+        throw new FileNotFoundException($"O aruivo {path} não existe!");
+
+    using var sr = new StreamReader(fi.FullName);
+    var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+        Delimiter = ";"
+    };
+    using var csvReader = new CsvReader(sr, csvConfig);
+
+    var registros = csvReader.GetRecords<Livro>();
+
+    foreach (var registro in registros)
+    {
+        Console.WriteLine($"nome:{registro.Titulo}");
+        Console.WriteLine($"Autor:{registro.Autor}");
+        Console.WriteLine($"preco:{registro.Preco}");
+        Console.WriteLine("--------");
+    }
+}
+
+
+static void LerCsvComClasse()
+{
+    var path = Path.Combine(
+        Environment.CurrentDirectory,
+        "Entrada",
+        "novos-usuarios.csv");
+    var fi = new FileInfo(path);
+
+    if (!fi.Exists)
+        throw new FileNotFoundException($"O aruivo {path} não existe!");
+
+    using var sr = new StreamReader(fi.FullName);
+    var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+    using var csvReader = new CsvReader(sr, csvConfig);
+
+    var registros = csvReader.GetRecords<Usuario>();
+
+    foreach (var registro in registros)
+    {
+        Console.WriteLine($"nome:{registro.Nome}");
+        Console.WriteLine($"email:{registro.Email}");
+        Console.WriteLine($"telefone:{registro.Telefone}");
+        Console.WriteLine("--------");
+    }
+}
+
+static void LerCSVComDynamic()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "produtos.csv");
+
+    var fi = new FileInfo(path);
+
+    if (!fi.Exists)
+        throw new FileNotFoundException($"O arquivo {path} não existe");
+
+    using var sr = new StreamReader(fi.FullName);
+    var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+    using var csvReader = new CsvReader(sr, csvConfig);
+
+    var registros = csvReader.GetRecords<dynamic>(); //não tem intelisense, precisa ser o nome exato
+
+    foreach (var registro in registros)
+    {
+        Console.WriteLine($"nome:{registro.Produto}");
+        Console.WriteLine($"marca:{registro.Marca}");
+        Console.WriteLine($"preco:{registro.Preco}");
+        Console.WriteLine("--------");
+    }
+
 }
