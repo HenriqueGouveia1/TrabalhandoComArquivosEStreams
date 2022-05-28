a577a1fe2022644501c8b41a2ec5efe6a517c54c
henriquegouveia1 <henriquegs002@gmail.com>
2022-05-28 13:09:31 -0300

    comentario

--- csvHelper/Model/Livro.cs
+++ csvHelper/Model/Livro.cs
@@ -1,6 +1,8 @@
 using CsvHelper.Configuration.Attributes;
 public class Livro
 {
+
+    //caso o arquivo não tenha cabeçalho podemos utilizar de mesmo modo os Attributes com Idexes
     [Name("titulo")]
     public string Titulo { get; set; }
     [Name("autor")]
