9462e0e4a886dab974e8b387b9b5e16f89ee44ba
henriquegouveia1 <henriquegs002@gmail.com>
2022-05-28 09:57:52 -0300

    Criando funcionalidade para ler um csv e imprimir

---
+++ csvStream/Program.cs
@@ -0,0 +1,25 @@
+﻿var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "usuarios-exportacao.csv");
+
+if (File.Exists(path))
+{
+    using var sr = new StreamReader(path);
+    var cabecalho = sr.ReadLine()?.Split(',');
+
+    while (true)
+    {
+        var registro = sr.ReadLine()?.Split(',');
+        if (registro == null) break;
+
+        if (cabecalho.Length != registro.Length)
+        {
+            Console.WriteLine("Arquivo Fora do Padrão");
+            break;
+        }
+
+        for (int i = 0; i < registro.Length; i++)
+        {
+            Console.WriteLine($"{cabecalho?[i]}: {registro[i]}");
+        }
+        Console.WriteLine("--------------------------------");
+    }
+}
