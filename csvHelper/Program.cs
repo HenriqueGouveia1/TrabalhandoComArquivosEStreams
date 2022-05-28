15abcf235709e7f9c9123dc6c72b833e0b1a4077
henriquegouveia1 <henriquegs002@gmail.com>
2022-05-28 10:42:29 -0300

    update

--- csvHelper/Program.cs
+++ csvHelper/Program.cs
@@ -2,5 +2,23 @@
 using CsvHelper.Configuration;
 using CsvHelper;
 
-var path = Path.Combine(Environment.CurrentDirectory,"Entrada","produtos.csv");
+var path = Path.Combine(Environment.CurrentDirectory, "Entrada", "produtos.csv");
 
+var fi = new FileInfo(path);
+
+if (!fi.Exists)
+    throw new FileNotFoundException($"O arquivo {path} não existe");
+
+using var sr = new StreamReader(fi.FullName);
+var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
+using var csvReader = new CsvReader(sr, csvConfig);
+
+var registros = csvReader.GetRecords<dynamic>(); //não tem intelisense, precisa ser o nome exato
+
+foreach (var registro in registros)
+{
+    Console.WriteLine($"nome:{registro.Nome}");
+    Console.WriteLine($"email:{registro.Email}");
+    Console.WriteLine($"telefone:{registro.Telefone}");
+    Console.WriteLine("--------");
+}
