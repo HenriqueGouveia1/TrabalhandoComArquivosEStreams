9d6660a1c76eafa651c3630ee4b40cd085accc43
henriquegouveia1 <henriquegs002@gmail.com>
2022-05-27 21:03:01 -0300

    método para ler infos dos arquivos

--- LerDiretórios/Program.cs
+++ LerDiretórios/Program.cs
@@ -1,9 +1,21 @@
 ﻿var path = @"C:\github projects\Curso Trabalhando com Arquivos e Streams em C#\Directory_and_DirectoryInfo\globo";
 
 LerDiretorios(path);
+LerArquivos(path);
 
 Console.WriteLine("Digite [enter] para finalizar");
 Console.ReadLine();
+
+static void LerArquivos(string path)
+{
+    var arquivos = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+    foreach (var arquivo in arquivos)
+    {
+        var fileInfo = new FileInfo(arquivo);
+        Console.WriteLine($"Nome: {fileInfo.Name}");
+    }
+}
 
 static void LerDiretorios(string path)
 {
