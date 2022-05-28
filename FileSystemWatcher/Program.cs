35ec6bd996732203a184bb1a361f9728954cbb86
henriquegouveia1 <henriquegs002@gmail.com>
2022-05-27 22:19:41 -0300

    Implementado monitoramento de arquivos

--- FileSystemWatcher/Program.cs
+++ FileSystemWatcher/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿var path = @"C:\github projects\Curso Trabalhando com Arquivos e Streams em C#\Directory_and_DirectoryInfo\globo";
+
+using var fsw = new FileSystemWatcher(path);
+
+fsw.Created += OnCreated;
+fsw.Deleted += OnDeleted;
+fsw.Renamed += OnRenamed;
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+Console.WriteLine("[Enter] Para Finalizar");
+Console.ReadLine();
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Objeto {e.Name} foi criado!");
+}
+
+
+void OnRenamed(object sender, RenamedEventArgs e)
+{
+    Console.WriteLine($"Objeto {e.Name} foi renomeado para {e.OldName}!");
+}
+
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+    Console.WriteLine($"Objeto {e.Name} foi deletado!");
+}
